4b92395ddfa2fe92f5c389c3400b15c787e5a86d
ckangwen <2437037469@qq.com>
2022-05-05 21:54:16 +0800

    feat(node): 更新exec方法

--- src/node/exec.ts
+++ src/node/exec.ts
@@ -2,5 +2,5 @@ import cp from "child_process";
 
 export const exec = (cmd: string, cwd: string = process.cwd()) => cp.execSync(cmd, {
   cwd,
-  stdio: ["pipe"],
+  stdio: ["pipe", "pipe", "pipe"],
 }).toString();
