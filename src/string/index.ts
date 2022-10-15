d6b9ecbf8d0f994eba8c41df4dac3f7785e105e5
ckangwen <2437037469@qq.com>
2022-10-15 20:52:41 +0800

    feat(string): add camelize & capitalize & hyphenate & toDisplayString

--- src/string/index.ts
+++ src/string/index.ts
@@ -1,74 +1,7 @@
 export * from "./ascii";
 export * from "./parseQueryString";
-
-/**
- * @description 如果字符串的长度小于指定的长度，那么在字符串的前面补充指定的字符串，直到字符串的长度达到指定的长度
- * @param { string } str - 需要处理的字符串
- * @param { number } len - 字符串期望的长度
- * @param { string } padStr - 补充的字符
- * @returns { string } 补充后的字符串
- */
-export const padStart = (string: string, length: number, pad = "0") => {
-  const s = `${string}`;
-  if (!s || s.length >= length) return string;
-  const padChar = pad.length <= 1 ? pad : pad.charAt(0);
-  return `${Array((length + 1) - s.length).join(padChar)}${string}`;
-};
-
-/**
- * @description 如果字符串的长度小于指定的长度，那么在字符串的后面补充指定的字符串，直到字符串的长度达到指定的长度
- * @param { string } str - 需要处理的字符串
- * @param { number } len - 字符串期望的长度
- * @param { string } padStr - 补充的字符
- * @returns { string } 补充后的字符串
- */
-export const padEnd = (string: string, length: number, pad = "0") => {
-  const s = `${string}`;
-  if (!s || s.length >= length) return string;
-
-  const padChar = pad.length <= 1 ? pad : pad.charAt(0);
-  return `${string}${Array((length + 1) - s.length).join(padChar)}`;
-};
-
-/**
- * @description 字符串的第一个字符转换为大写
- * @param { string } str - 需要处理的字符串
- * @returns { string } 处理后的字符串
- */
-export const upperFirst = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
-
-/**
- * @description 字符串的第index个字符转换为大写
- * @param { string } str - 需要处理的字符串
- * @param { number } index - 字符串的第index个字符
- * @returns { string } 处理后的字符串
- */
-export const upperAt = (str: string, index: number) => {
-  if (index >= str.length) return str;
-
-  if (index === 0) return upperFirst(str);
-
-  const newIndex = index < 0 ? str.length + index : index;
-  return str.slice(0, newIndex) + upperFirst(str.slice(newIndex));
-};
-
-/**
- * @description 字符串的第一个字符转换为小写
- * @param { string } str - 需要处理的字符串
- * @returns { string } 处理后的字符串
- */
-export const lowerFirst = (str: string) => str.charAt(0).toLowerCase() + str.slice(1);
-
-/**
- * @description 字符串的第index个字符转换为小写
- * @param { string } str - 需要处理的字符串
- * @param { number } index - 字符串的第index个字符
- */
-export const lowerAt = (str: string, index: number) => {
-  if (index >= str.length) return str;
-
-  if (index === 0) return lowerFirst(str);
-
-  const newIndex = index < 0 ? str.length + index : index;
-  return str.slice(0, newIndex) + lowerFirst(str.slice(newIndex));
-};
+export * from "./cacheStringFunction";
+export * from "./camelize";
+export * from "./capitalize";
+export * from "./hyphenate";
+export * from "./toDisplayString";
---
+++ src/string/toDisplayString.ts
@@ -0,0 +1,51 @@
+/* eslint-disable no-nested-ternary */
+import {
+  isStr,
+  isArr,
+  isObj,
+  isFn,
+  isMap,
+  isSet,
+  isPlainObj,
+} from "../is/index";
+
+const defaultReplacer = (_key: string, value: any): any => {
+  if (isMap(value)) {
+    return {
+      [`Map(${value.size})`]: [ ...value.entries() ].reduce((entries, [key, val]) => {
+        // eslint-disable-next-line no-param-reassign
+        (entries as any)[`${key} =>`] = val;
+        return entries;
+      }, {}),
+    };
+  }
+  if (isSet(value)) {
+    return {
+      [`Set(${value.size})`]: [ ...value.values() ],
+    };
+  }
+  if (isObj(value) && !isArr(value) && !isPlainObj(value)) {
+    return String(value);
+  }
+  return value;
+};
+
+/**
+ * convert value to display on the dom
+ * @example
+ * toDisplayString("foo") // foo
+ * toDisplayString(10) // 10
+ * toDisplayString({ a: 1 }) // { "a": 1 }
+ */
+export const toDisplayString = (
+  val: unknown,
+  replacer: typeof defaultReplacer = defaultReplacer,
+): string => (isStr(val)
+  ? val
+  : val == null
+    ? ""
+    : isArr(val)
+      || (isObj(val)
+        && (val.toString === Object.prototype.toString || !isFn(val.toString)))
+      ? JSON.stringify(val, replacer, 2)
+      : String(val));
