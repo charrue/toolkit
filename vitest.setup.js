6741bd172a1ff7faa03fc58f3fdefed45449df03
ckangwen <2437037469@qq.com>
2022-11-01 12:54:38 +0800

    chore: lint & update unbuild config

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,7 +1,7 @@
 import "whatwg-fetch";
 
 /** Encodes a string as base64 format */
-global.btoa = (str) => Buffer.from(str, 'binary').toString('base64');
+global.btoa = (str) => Buffer.from(str, "binary").toString("base64");
 
 /** Decodes a base64 encoded string */
-global.atob = (str) => Buffer.from(str, 'base64').toString('binary');
+global.atob = (str) => Buffer.from(str, "base64").toString("binary");
