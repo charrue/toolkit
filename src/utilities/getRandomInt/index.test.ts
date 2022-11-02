6487f337411a98e1e8d42d5832011992cd222644
ckangwen <2437037469@qq.com>
2022-11-02 10:24:46 +0800

    feat(getRandomInt): add minimun value

--- src/utilities/getRandomInt/index.test.ts
+++ src/utilities/getRandomInt/index.test.ts
@@ -1,6 +1,11 @@
 import { test, expect } from "vitest";
 import { getRandomInt } from "./index";
 
-test("getRandomInt", () => {
+test("getRandomInt(max)", () => {
   expect(getRandomInt(1000)).lessThan(1000);
+});
+
+test("getRandomInt(max, min)", () => {
+  expect(getRandomInt(100, 10)).lessThanOrEqual(100);
+  expect(getRandomInt(100, 10)).greaterThanOrEqual(10);
 });
--- src/utilities/getRandomInt/index.ts
+++ src/utilities/getRandomInt/index.ts
@@ -2,4 +2,5 @@
  * Generating a random int in range (0, max - 1)
  * @param max {number}
  */
-export const getRandomInt = (max: number) => Math.floor(Math.random() * Math.floor(max));
+// eslint-disable-next-line max-len
+export const getRandomInt = (max: number, min = 0) => min + Math.floor(Math.random() * Math.floor(max));
