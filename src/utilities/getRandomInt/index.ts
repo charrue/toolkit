2389b4326b6705948cebdf228571b81920cd91ad
ckangwen <2437037469@qq.com>
2022-11-12 08:48:03 +0800

    refactor(utilities): remove getRandomInt

--- src/utilities/getRandomInt/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Generating a random int in range (0, max - 1)
- * @param max {number}
- */
-// eslint-disable-next-line max-len
-export const getRandomInt = (max: number, min = 0) => min + Math.floor(Math.random() * Math.floor(max));
--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,3 +1,2 @@
 export * from "./retry";
 export * from "./isEmpty";
-export * from "./getRandomInt/index";
